b263035e6031d25f2722d91cf8e93f821d0f8331
mshenawy <m.alshenawy91@gmail.com>
2017-11-22 14:16:25 +0200

    solve act as votable issue

--- db/migrate/20171122115527_acts_as_votable_migration.rb
+++ db/migrate/20171122115527_acts_as_votable_migration.rb
@@ -1,9 +1,8 @@
-
-class ActsAsVotableMigration < ActiveRecord::Migration
+class ActsAsVotableMigration < ActiveRecord::Migration[4.2]
   def self.up
     create_table :votes do |t|
 
-
+      t.references :votable, :polymorphic => true
       t.references :voter, :polymorphic => true
 
       t.boolean :vote_flag
@@ -11,11 +10,6 @@ class ActsAsVotableMigration < ActiveRecord::Migration
       t.integer :vote_weight
 
       t.timestamps
-    end
-
-    if ActiveRecord::VERSION::MAJOR < 4
-      add_index :votes, [:votable_id, :votable_type]
-      add_index :votes, [:voter_id, :voter_type]
     end
 
     add_index :votes, [:voter_id, :voter_type, :vote_scope]
