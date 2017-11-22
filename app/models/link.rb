bca71fc49262f1f2a168866be8263f7854c0ad12
mshenawy <m.alshenawy91@gmail.com>
2017-11-22 15:23:58 +0200

    solve url issue

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,6 @@
 class Link < ApplicationRecord
 	validates :title,  presence: true
-	validates :url,  presence: true
+	validates :url,  presence: true, :format => URI::regexp(%w(http https))
 	acts_as_votable
 	belongs_to :user
 	has_many :comments
